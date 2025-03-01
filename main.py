6411ae46ae27a201866f336fa54a4a0b69cc4f51
Raeo <mail@raeonaut.space>
2025-03-01 11:22:01 +0700

    feat: limit time frame to 60 fps

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+	fps_clock = pygame.time.Clock()
+	dt = 0
+
 	while(True):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -12,6 +15,9 @@ def main():
 
 		screen.fill((0, 0, 0))
 		pygame.display.flip()
+
+		# limit timeframe to 60 fps
+		dt = fps_clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
